45cfca7db2f6fe1009b6b8993a15cdc78002942d
Milad Mazaheri <milad.kbr@outlook.com>
2021-11-02 15:14:39 +0330

    Use ?route in older version instead of ?locomotion

--- Program.cs
+++ Program.cs
@@ -14,10 +14,12 @@ namespace OpenCardApiClient
 {
     class Program
     {
+
+        //TODO : Use ?route in older version instead of ?locomotion
         //private static string BASE_URL = "https://localhost/";
-        private static string BASE_URL = "https://lift-store.ir/";
+        private static string BASE_URL = "https://style-store.ir/hatra/";
         //private static string key = "NjXcqVVthcBo1wvqhAaAY7tfZDUmM3tOQ7aOKdPfllsxGp8HLQSG4WTZHbRyLoxV6GziUktVq5gd3ivC2qNpMVnirHlRib5oEDE9FwqSbfx15swQkjTOJLVjhjwOOD3n2OSVjHkssh7mCan5Qg4k5h3Mbl4pN2xvrwHhNl4nToRf8CM8ib9ToLKF6L0muTb5bMHw7mVeYz1XFTiNKm1ejDaZnwnMzbkbNr5x2Sg3uMzOzac3hTQGydJ24In1RX3n";
-        private static string key = "vrNHX91XzHPKrswdgyY70OgHRpFAxFjb68B2ILcCiew8sjJu4GNvdGKn2cgyQFXcqadqrzz8cS77sJOGG8zcdv0pK6ZRalWbeRVPh9IGFcdZelu8jFv3D7qbj9N4m8E06go8Jpzxzb4Ukqu756ElVmH7mqHZuAshMAjux58r7xKG6axRWpwdSwsGomgnMyRHTnP5qbyoqf3x5YRMEhQuN9TWgyCJr0e2tBp7vQmXdQWVu8TiZkUgIbhSxIeBbWmp";
+        private static string key = "Z95K5AYa08eufY4bO5uCqHI6iOczClrSZTU8V3kJXtleMN3J3dvzxHgX8hyeUFmy8FP5TbRJoJJldedlXGMMNRstympdwf5VsZ9BYZSp9ZnecKKX4Zcsvt4KVFtHfdVMnPLQmZouWemwDWo1KDCTIi6cUjmTSk6wfjIt3kYmmQ57acqwnOJExgHuJw6qwdbMEVxvEn0gpI6Pafv5A5YJbKHlYcz5VwBLLFGBPi8Y6fuzrnd4814lKcUuz1zr5gB4";
         static async Task Main(string[] args)
         {
             Console.WriteLine("OpenCart Api Client");
@@ -68,7 +70,7 @@ namespace OpenCardApiClient
 
         private static async Task UpdateProductsAsync(HttpClient http, string apiToken)
         {
-            var route = $"{BASE_URL}index.php?route=api/hatra/updateProduct&token={apiToken}";
+            var route = $"{BASE_URL}index.php?locomotion=api/hatra/updateProduct&token={apiToken}";
             var products = new ProductViewModel[]
             {
                 new ProductViewModel { Id= 53,Price=0,Quantity=2522222  },
@@ -98,35 +100,35 @@ namespace OpenCardApiClient
         }
         private static async Task GetProductsFromHatraApiAsync(HttpClient http, string apiToken)
         {
-            var route = $"{BASE_URL}index.php?route=api/hatra/getproducts&start=0&limit=10&token={apiToken}";
+            var route = $"{BASE_URL}index.php?locomotion=api/hatra/getproducts&start=0&limit=10&token={apiToken}";
             var response = await http.GetAsync(route);
             var content = await response.Content.ReadAsStringAsync();
             Console.WriteLine("products by hatra api :\n" + JToken.Parse(content).ToString(Formatting.Indented) + "\n\n\n");
         }
         private static async Task GetCustomersFromHatraApiAsync(HttpClient http, string apiToken)
         {
-            var route = $"{BASE_URL}index.php?route=api/hatra/getcustomers&start=0&limit=10&token={apiToken}";
+            var route = $"{BASE_URL}index.php?locomotion=api/hatra/getcustomers&start=0&limit=10&token={apiToken}";
             var response = await http.GetAsync(route);
             var content = await response.Content.ReadAsStringAsync();
             Console.WriteLine("customers by hatra api :\n" + JToken.Parse(content).ToString(Formatting.Indented) + "\n\n\n");
         }
         private static async Task GetOrderStatusesFromHatraApiAsync(HttpClient http, string apiToken)
         {
-            var route = $"{BASE_URL}index.php?route=api/hatra/getorderstatuses&token={apiToken}";
+            var route = $"{BASE_URL}index.php?locomotion=api/hatra/getorderstatuses&token={apiToken}";
             var response = await http.GetAsync(route);
             var content = await response.Content.ReadAsStringAsync();
             Console.WriteLine("order statuses by hatra api :\n" + JToken.Parse(content).ToString(Formatting.Indented) + "\n\n\n");
         }
         private static async Task GetOrdersFromHatraApiAsync(HttpClient http, string apiToken)
         {
-            var route = $"{BASE_URL}index.php?route=api/hatra/getorders&start=0&limit=10&startDate=2021-01-01&endDate=2021-12-29&status=1&token={apiToken}"; /*&status = 1*/
+            var route = $"{BASE_URL}index.php?locomotion=api/hatra/getorders&start=0&limit=10&startDate=2021-01-01&endDate=2021-12-29&status=1&token={apiToken}"; /*&status = 1*/
             var response = await http.GetAsync(route);
             var content = await response.Content.ReadAsStringAsync();
             Console.WriteLine("orders by hatra api :\n" + JToken.Parse(content).ToString(Formatting.Indented) + "\n\n\n");
         }
         public static async Task<string> LoginAndGetTokenAsync(HttpClient http)
         {
-            var loginUrl = $"{BASE_URL}index.php?route=api/hatra/login";
+            var loginUrl = $"{BASE_URL}index.php?locomotion=api/hatra/login";
 
             var username = "Default";
             var response = await http.PostAsync(loginUrl, new FormUrlEncodedContent(new[] { new KeyValuePair<string?, string?>("key", key), new KeyValuePair<string?, string?>("username", username) }));
